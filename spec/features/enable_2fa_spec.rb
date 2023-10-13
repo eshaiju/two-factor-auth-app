e192d6c3e4b6ffefc596d5b24b2c0dac25a78500
Shaiju Edakulangara <eshaiju@gmail.com>
2023-10-13 20:56:03 +0530

    Option to enabling two-factor authentication

---
+++ spec/features/enable_2fa_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'capybara/rspec'
+
+describe 'Enabling Two-Factor Authentication' do
+  it 'enables 2FA for the user' do
+    user = User.create(email: 'user@example.com', password: 'password')
+    login_user(user.email, user.password)
+
+    click_link 'Enable Two-Factor Authentication'
+
+    fill_in 'code', with: valid_2fa_code(user)
+    click_button 'Enable 2FA'
+
+    user.reload
+    expect(page).to have_content('Two-Factor Authentication is Enabled')
+
+    expect(user.two_factor_enabled).to be(true)
+  end
+
+  private
+
+  def valid_2fa_code(user)
+    totp = ROTP::TOTP.new(user.secret_key)
+    totp.now
+  end
+end
